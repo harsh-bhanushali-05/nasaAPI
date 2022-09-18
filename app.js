34f69ad0ddcee6b2394fbab7504a962d326b6b43
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2022-09-19 00:17:08 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -7,21 +7,72 @@ app.use(express.static("public"));
 var titles="";
 var description="";
 var img="";
-var date="2022-09-07";
-https.get("https://api.nasa.gov/planetary/apod?api_key=3SozCbLWWgp0Z1nWRFStdAZyTTRQPr5ZlXnqx16K&date="+date,function(ress){
-    ress.on("data",function(data){
-        const dataJ=JSON.parse(data);
-         titles=dataJ.title;
-         description=dataJ.explanation;
-         img=dataJ.url;
-        console.log(description);
-        
-    });
-});
+const d = new Date();
+let day = d.getDate();
+let month=d.getMonth();
+let year=d.getFullYear();
+var date=year+"-"+month+"-"+day;
+var nodays=[31,28,31,30,31,30,31,31,30,31,30,31];
+
+function reduce_date(){
+if(day==1 && month==1){
+    year=year-1;
+    day=31;
+    month=12;
+}
+else if(day==1){
+    month=month-1;
+    day=nodays[month-1];
+}
+else{
+    day=day-1;
+}
+date=year+"-"+month+"-"+day;
+}
+
+function increase_date(){
+    if(day==31 && month==12){
+        year=year+1;
+        day=1;
+        month=01;
+    }
+    else if(day==nodays[month]){
+        month=month+1;
+        day=nodays[month-1];
+    }
+    else{
+        day=day+1;
+    }
+    date=year+"-"+month+"-"+day;
+}
+
 app.get("/",function(req,res){
+    res.redirect("/"+date);
+});
+app.get("/:date",function(req,res){
+    https.get("https://api.nasa.gov/planetary/apod?api_key=3SozCbLWWgp0Z1nWRFStdAZyTTRQPr5ZlXnqx16K&date="+date,function(ress){
+        ress.on("data",function(data){
+            const dataJ=JSON.parse(data);
+             titles=dataJ.title;
+             description=dataJ.explanation;
+             img=dataJ.url;
+            console.log(description); 
+        });
+    });
     res.render("index",{description:description,title:titles,img:img});
+});
+app.post("/prev",function(req,res){
+    console.log("the prev button was pressed");
+    reduce_date();
+    res.redirect("/"+date);
+});
+app.post("/next",function(req,res){
+    console.log("the next button was pressed");
+    increase_date();
+    res.redirect("/"+date);
 });
 // what are we trying to do --- the main page will redirect us to a new page with the date in the link if we click the previous button the link will be updated to match the date needed 
 app.listen(3000,function(){
 console.log("works");
 });
+
